af8e6b184188034a0d9582afae1287cf08aa6ee4
pvq2k2 <vanquyet2392002e@gmail.com>
2022-08-10 10:40:56 +0700

    [Server] feat: Create fake data

---
+++ server/data/static.js
@@ -0,0 +1,62 @@
+const books = [
+    {
+        id: 1,
+        name: 'Tắt đèn',
+        genre: 'Tiểu thuyết',
+        authorId: 1
+    },
+    {
+        id: 2,
+        name: 'Chí phèo',
+        genre: 'Truyện ngắn',
+        authorId: 2
+    },
+    {
+        id: 3,
+        name: 'Số đỏ',
+        genre: 'Tiểu thuyết',
+        authorId: 3
+    },
+    {
+        id: 4,
+        name: 'Đời thừa',
+        genre: 'Truyện ngắn',
+        authorId: 2
+    },
+    {
+        id: 5,
+        name: 'Sống mòn',
+        genre: 'Tiểu thuyết',
+        authorId: 2
+    },
+    {
+        id: 6,
+        name: 'Kỹ nghệ lấy Tây',
+        genre: 'Phóng sự',
+        authorId: 3
+    }
+];
+
+const authors = [
+    {
+        id: 1,
+        name: 'Ngô Tất Tố',
+        age: 127
+    },
+    {
+        id: 2,
+        name: 'Nam Cao',
+        age: 106
+    },
+    {
+        id: 3,
+        name: 'Vũ Trọng Phụng',
+        age: 109
+    }
+];
+
+
+module.exports = {
+    books,
+    authors
+}
